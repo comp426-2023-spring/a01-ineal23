29578c3d865e561f8515d3c517bd8c384041d26f
Isaiah Neal <ineal23@email.unc.edu>
2023-02-08 13:21:56 -0500

    a1

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ fs.readFile('./public/index.html', 'utf8', (err, data) => {
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
-  res.end('./public/index.html\n');
+  res.end(data);
 });
 
 server.listen(port, () => {
