013f1f8639cd2a0f3c823f4776fafc87b303ed1a
Isaiah Neal <ineal23@email.unc.edu>
2023-02-08 13:11:38 -0500

    a1

--- server.js
+++ server.js
@@ -41,13 +41,15 @@ fs.readFile('./public/index.html', 'utf8', (err, data) => {
 // 1. status code 200, 
 // 2. set a header with content type `text/html`, and 
 // 3. end with the data that you are reading in from ./public/index.html.
-http.createServer(function (req, res) {
+const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
   res.end('./public/index.html\n');
-}).listen(port);
+});
 
-console.log(`Server listening on port ${port}`);
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
 
 // Start the `server` const listening on the port defined by argument in your `port` const. 
 // Put the exact message `Server listening on port ${port}` on the console log. 
